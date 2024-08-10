a7c11da507a42f6e715467700599ddaaafdc6cee
Gupteshwar Nath Prajapati <155339550+GupteshwarNath@users.noreply.github.com>
2024-08-10 12:40:20 +0000

    conatainer config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId=144553
+let accountEmail="gupteshwardln123@gmail.com"
+var accountpassword="123456"
+accountcity="Bhilai"
+let accountstate=
+
+// accountId=564789 Not Allowed
+
+/*
+prefer not to use var
+because of issue in block scope and functional 
+scope
+*/
+
+accountEmail="gnp20022000@gmail.com"
+accountpassword="321467"
+accountcity="Durg"
+accountstate=
+
+console.log(accountId,accountEmail,accountpassword,accountcity);
+console.table([accountId,accountEmail,accountpassword,accountcity,accountstate]);
