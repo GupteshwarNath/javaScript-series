76b354fdbdca4855a8024688040c7d2fc74ba62e
Gupteshwar Nath Prajapati <155339550+GupteshwarNath@users.noreply.github.com>
2024-08-11 13:54:05 +0000

    dataypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat alljs code as newer version
+
+// alert(3+3) we are using nodejs, not browser
+
+console.log(3
+    +
+3)//code readability should be high
+console.log("gupteshwar");
+let name="gupteshwar"
+let age=18
+let isLoggedId=false
+//number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbole
+// object
+console.log(typeof null);
+
