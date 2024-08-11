c43b43bddd04b26197042955518e351455bac391
Gupteshwar Nath Prajapati <155339550+GupteshwarNath@users.noreply.github.com>
2024-08-11 14:54:05 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score=55
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+
+// "33" =>33
+// "33abc" =>NaN
+// true =>1; false =>0
+
+let isLoggedIn="Gupteshwar"
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let someNumber=33
+let stringNumber=string(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
