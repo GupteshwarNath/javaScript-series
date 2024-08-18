33e14a55d32f4901fca9abdf52346ec1878da681
Gupteshwar Nath Prajapati <155339550+GupteshwarNath@users.noreply.github.com>
2024-08-18 06:39:17 +0000

    ConversionOperation done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,26 +1,63 @@
-let score=55
+ let score=55
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let valueInNumber=Number(score)
-console.log(typeof valueInNumber);
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
-console.log(valueInNumber);
-
-// "33" =>33
-// "33abc" =>NaN
-// true =>1; false =>0
+// // "33" =>33
+// // "33abc" =>NaN
+// // true =>1; false =>0
 
 let isLoggedIn="Gupteshwar"
 
-let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
-let someNumber=33
-let stringNumber=string(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//  let someNumber=33
+
+ let stringNumber=string(someNumber)
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+let value=3
+let negvalue=-value
+// console.log(negvalue);
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1="hello"
+let str2="gupteshwar"
+
+let str3=str1+str2
+// console.log(str3)
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+// console.log(1+2+"2")
+// console.log((3+4)*5%3)
+// console.log(+true)
+// console.log(+"")
+
+let num1,num2,num3
+num1=num2=num3=2+2
+console.log(num1)
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter)
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
 
 
 
