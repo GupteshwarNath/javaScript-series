3cb436d46424bd5238692898dda7f939f9e6ac97
Gupteshwar Nath Prajapati <155339550+GupteshwarNath@users.noreply.github.com>
2024-09-20 07:13:10 +0000

    Array1

---
+++ 02_basics/array1.js
@@ -0,0 +1,36 @@
+const myArr=[0,1,2,3,4,5]
+const myHeroies=["shaktiman","naagraj"]
+
+// const myArr2=new Array(1,2,3,4)
+//console.log(myArr[0]);
+
+// Array Method
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop(7)
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(19))
+
+// const newArr=myArr.join()
+
+// console.log(myArr);
+// console.log(newArr);
+
+// slice, splice
+
+console.log("A",myArr)
+
+const myArr1=myArr.slice(1,3)
+
+console.log(myArr1);
+console.log("B",myArr);
+
+const myArr2=myArr.splice(1,3)
+console.log("c",myArr);
+console.log(myArr2);
+
