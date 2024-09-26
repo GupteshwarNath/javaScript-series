984925a1c717a43e78276bdf466343d15dc38b2b
Gupteshwar Nath Prajapati <155339550+GupteshwarNath@users.noreply.github.com>
2024-09-26 11:06:33 +0000

    Object1

---
+++ 02_basics/object1.js
@@ -0,0 +1,12 @@
+const JsUser={
+    name:"Gupteshwar"
+    "full_name": "Gupteshwar Nath Prajapati",
+    age: 24,
+    location:"Bhilai",
+    email:"hitesh@google.com"
+    isLoggedin: false,
+    lastLoginDays:["Monday","saturday"]
+}
+console.log(JsUser.email)
+console.log(JsUser["email"])
+console.log(JsUser["full name"])
