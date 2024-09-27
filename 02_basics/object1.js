8e300427133dda17257e22e9db21c162d37c1611
Gupteshwar Nath Prajapati <155339550+GupteshwarNath@users.noreply.github.com>
2024-09-27 05:48:23 +0000

    Object1

--- 02_basics/object1.js
+++ 02_basics/object1.js
@@ -1,12 +1,37 @@
+// singleton
+//object.create
+//object literals
+
+const mySym = Symbol("key1")
+
+
 const JsUser={
-    name:"Gupteshwar"
-    "full_name": "Gupteshwar Nath Prajapati",
+    name:"Gupteshwar",
+    "full name": "Gupteshwar Nath Prajapati",
+    [mySym]: "mykey1",
     age: 24,
     location:"Bhilai",
-    email:"hitesh@google.com"
+    email:"gupteshwardln123@gmail.com",
     isLoggedin: false,
     lastLoginDays:["Monday","saturday"]
 }
-console.log(JsUser.email)
-console.log(JsUser["email"])
-console.log(JsUser["full name"])
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
+
+JsUser.email="gupteshwardln@gmail.com"
+// Object.freeze(JsUser);
+JsUser.email="gupteshwardln@gmail.com"
+// console.log(JsUser);
+
+JsUser.greeting=function(){
+    console.log("Hello JsUser");
+}
+JsUser.greetingTwo=function(){
+console.log('Hello JsUser,${this.name}');
+}
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
+
+// console.log(JsUser.greeting);
